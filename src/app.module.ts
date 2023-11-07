b4b6152c17faf0be6817d0eb36f7365da5b09901
Maria-Lavinia Otelea <mlo@frankly.dk>
2023-11-07 12:55:20 +0100

    setting up postegress connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('localhost'),
+        port: +configService.get<number>('5432'),
+        username: configService.get('postgres'),
+        password: configService.get('060994'),
+        database: configService.get('db_lottie_animations'),
+        autoLoadEntities: true,
+        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
